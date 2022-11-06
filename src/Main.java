64ed390330d709fcf8013b9440bb126c9e2f8390
Sveta <sveta.zheleznova.03@mail.ru>
2022-11-06 22:16:49 +0300

    add 2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
         System.out.println(solutions(1, 0, 0));
         System.out.println(solutions(1, 0, 1));
 
+        System.out.println("Задание 2");
+        System.out.println(findZip("all zip files are zipped"));
+        System.out.println(findZip("all zip files are compressed"));
+
     }
 
     // возвраащаем число решений квадратного уравнения ax^2+bx+c=0
@@ -16,6 +20,18 @@ public class Main {
             return 1;
         } else {
             return 2;
+        }
+    }
+    // возвращаем позицию второго вхождения zip в строку
+    public static int findZip(String s) {
+        int firstZip = s.indexOf("zip"); // находим позицию первого вхождения zip
+        String sWithoutFirstZip = s.substring(firstZip + 3); // обрезаем строку(убераем всё до zip + само zip)
+        int positionWithoutFirstZip = sWithoutFirstZip.indexOf("zip"); // находим вхождение zip в обрезаннос строке
+        if (positionWithoutFirstZip == -1){
+            return  -1; // если zip не найдено
+        } else {
+            return positionWithoutFirstZip + firstZip + 3;
+            // позиция первого вхождения zip + длина zip + позиция второго вхождения в обрезанной строке
         }
     }
 }
