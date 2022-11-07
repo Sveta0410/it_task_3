96ed7c4e4bf885d544b2f1dc27943540755b107a
Sveta <sveta.zheleznova.03@mail.ru>
2022-11-07 11:54:47 +0300

    add 9 + make code more beautiful

--- src/Main.java
+++ src/Main.java
@@ -34,14 +34,14 @@ public class Main {
         System.out.println(isValidHexCode("CD5C5C"));
 
         System.out.println("Задание 6");
-        int[] array1 = new int[] {1, 3, 4, 4, 4};
-        int[] array2 = new int[] {2, 5, 7};
+        int[] array1 = new int[]{1, 3, 4, 4, 4};
+        int[] array2 = new int[]{2, 5, 7};
         System.out.println(same(array1, array2));
-        int[] array3 = new int[] {9, 8, 7, 6};
-        int[] array4 = new int[] {4, 4, 3, 1};
+        int[] array3 = new int[]{9, 8, 7, 6};
+        int[] array4 = new int[]{4, 4, 3, 1};
         System.out.println(same(array3, array4));
-        int[] array5 = new int[] {2};
-        int[] array6 = new int[] {3, 3, 3, 3, 3};
+        int[] array5 = new int[]{2};
+        int[] array6 = new int[]{3, 3, 3, 3, 3};
         System.out.println(same(array5, array6));
 
         System.out.println("Задание 7");
@@ -56,12 +56,17 @@ public class Main {
         System.out.println(longestZero("100100100"));
         System.out.println(longestZero("11111"));
 
+        System.out.println("Задание 9");
+        System.out.println(nextPrime(12));
+        System.out.println(nextPrime(24));
+        System.out.println(nextPrime(11));
+
     }
 
     // возвраащаем число решений квадратного уравнения ax^2+bx+c=0
     public static int solutions(int a, int b, int c) {
-        double discriminant = b*b - 4*a*c;
-        if (discriminant < 0){
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0) {
             return 0;
         } else if (discriminant == 0) {
             return 1;
@@ -69,39 +74,42 @@ public class Main {
             return 2;
         }
     }
+
     // возвращаем позицию второго вхождения zip в строку
     public static int findZip(String s) {
         int firstZip = s.indexOf("zip"); // находим позицию первого вхождения zip
         String sWithoutFirstZip = s.substring(firstZip + 3); // обрезаем строку(убераем всё до zip + само zip)
         int positionWithoutFirstZip = sWithoutFirstZip.indexOf("zip"); // находим вхождение zip в обрезаннос строке
-        if (positionWithoutFirstZip == -1){
-            return  -1; // если zip не найдено
+        if (positionWithoutFirstZip == -1) {
+            return -1; // если zip не найдено
         } else {
             return positionWithoutFirstZip + firstZip + 3;
             // позиция первого вхождения zip + длина zip + позиция второго вхождения в обрезанной строке
         }
     }
+
     // проверяем, можно ли записать число как сумму его множителей, исключая само число (6=1+2+3)
     public static boolean checkPerfect(int number) {
         int sum = 1; // т.к. все числа делятся на 1
         // 6=1*2*3, 6%2==0 => 6%(6/3)==0
-        for (int i = 2; i <= Math.sqrt(number); i++){
-            if (number % i == 0){
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
                 sum += i;
                 // если множитель равен корню исходного числа, то его "пару" добавлять не надо, т.к. это то же число
-                if (i != Math.sqrt(number)){
+                if (i != Math.sqrt(number)) {
                     sum += number / i;
                 }
             }
         }
         return sum == number;
     }
+
     // меняем в строке первый и последний символ
     public static String flipEndChars(String myString) {
-        if (myString.length() < 2){
+        if (myString.length() < 2) {
             // если длина строки меньше 2, возвращаем "несовместимо."
             return "Incompatible.";
-        } else if (myString.charAt(0) == myString.charAt(myString.length() - 1)){
+        } else if (myString.charAt(0) == myString.charAt(myString.length() - 1)) {
             // если первый и последний символы совпадают, возвращаем "два-это пара."
             return "Two's a pair.";
         } else {
@@ -111,35 +119,39 @@ public class Main {
                     myString.charAt(0);
         }
     }
+
     // определяем, является ли строка допустимым шестнадцатеричным кодом
     public static boolean isValidHexCode(String myString) {
-        if (myString.charAt(0) != '#'){
+        if (myString.charAt(0) != '#') {
             return false;
-        } else if (myString.length() != 7){
+        } else if (myString.length() != 7) {
             // 7 = 6 + #
             return false;
-        } else if (!myString.substring(1, myString.length()).matches("[A-Fa-f0-9]{6}")){
+        } else if (!myString.substring(1, myString.length()).matches("[A-Fa-f0-9]{6}")) {
             //регулярное выражение (цифры 0-9 или буквы A-F, a-f,  кол-во символов 6) [рассматриваем подстроку(без #)]
             // !ВАЖНО! рассматриваем противоположное событие (строка не подошла по параметрам -> возвращаем false)
             return false;
-        } else{
+        } else {
             return true;
         }
     }
+
     // возвращаем true, если два массива имеют одинаковое количество уникальных элементов, и false в противном случае
     public static boolean same(int[] arr1, int[] arr2) {
         return removeDublicates(arr1).size() == removeDublicates(arr2).size();
     }
+
     // вспомогательная функция, позволяющая создать из исходного массива список с уникальными элементами
     public static List<Integer> removeDublicates(int[] arr) {
         List<Integer> newArr = new ArrayList<Integer>();
-        for (int num : arr){
-            if (!(newArr.contains(num))){
+        for (int num : arr) {
+            if (!(newArr.contains(num))) {
                 newArr.add(num);
             }
         }
         return newArr;
     }
+
     // возвращаем true, если это число Капрекара, и false, если это не так
     // (число, кот. после возведения в квадрат и разбиения на две части равно сумме двух полученных новых чисел)
     public static boolean isKaprekar(int number) {
@@ -149,20 +161,39 @@ public class Main {
         if (numLen == 1) {
             // если количество цифр == 1
             return nSquare == number;
-        } else{
+        } else {
             int left = Integer.parseInt(numStr.substring(0, numLen / 2));
             int right = Integer.parseInt(numStr.substring(numLen / 2));
             return left + right == number;
         }
     }
+
     // возвращаем самую длинную последовательность последовательных нулей в двоичной строке
     public static String longestZero(String number) {
-        if (number.contains("0")){
+        if (number.contains("0")) {
             String[] zeroes = number.split("1+");
             Arrays.sort(zeroes);
             return zeroes[zeroes.length - 1];
         } else {
             return "";
         }
+    }
+
+    // возвращаем следующее простое число. если число простое, возвращаем само число
+    public static int nextPrime(int number) {
+        for (int i = number; ; i++){
+            if (isPrime(i)){
+                return i;
+            }
+        }
+    }
+    // вспомогательная функция, которая проверяет, является ли число простым
+    public static boolean isPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
